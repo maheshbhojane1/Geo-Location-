ed06c665c08e5738064a4005e9559cebdeec2a0e
Mahesh Bhojane <152635189+maheshbhojane1@users.noreply.github.com>
2024-01-19 20:18:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-const btn = document.querySelector('.geo-btn')
-const details = document.querySelector('.showDetails')
+const btn = document.querySelector(".geo-btn");
+const details = document.querySelector(".showDetails");
+const fullAdress = document.querySelector(".fullAddress");
 
-btn.addEventListener('click', ()=>{
+let apiEndpoint = "https://api.opencagedata.com/geocode/v1/json"
+let apiKey = "40f15dac841b4f388aaea6f32a81ec59"
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(
-            
-            (location)=>{
-                const {latitude, longitude} = location.coords
-                details.textContent = `Latitude ${latitude} &
-                Longitude ${longitude}`
-            },
-            (error)=>{
-                details.textContent = error.message
-                console.log(error.message)
-            })
+const getCurrentAdress = async (latitude,longitude)=>{
+    let queary = `${latitude}, ${longitude}`
+    let apiUrl = `${apiEndpoint}?key=${apiKey}&q=${queary}&pretty=1`
+    
+    try {
+        let  responce = await fetch(apiUrl)
+        let data = await responce.json()
+
+        const {city, state, postcode, country} = data.results[0].components;
+
+        fullAdress.textContent = `User Adress: ${city}, ${postcode}, ${state}, ${country}`
+    } catch (error) {
+        alert(error)
     }
-})
+}
+
+btn.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        details.textContent = `the latitude ${latitude} and longitude
+        ${longitude}`
+
+        getCurrentAdress(latitude, longitude)
+      },
+      (error) => {
+        details.textContent = error.message;
+      }
+    );
+  }
+});
